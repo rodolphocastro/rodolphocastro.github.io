da4a3ce9a185a33b157b8e0816b8e093a92b96ea
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-21 16:53:46 -0300

    Realizar scaffold da view de Projetos

---
+++ src/views/Projetos.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi voluptatum repellendus et vitae consectetur pariatur soluta sed! Libero accusamus obcaecati esse iste facere? Repellat illum aperiam voluptatem nisi similique expedita.</p>
+  </section>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import { mapActions } from 'vuex'
+
+@Component({
+  name: 'projetos',
+  methods: { ...mapActions('projetos', ['fetchRepositories']) }
+})
+export default class Projetos extends Vue {
+  fetchRepositories!: () => Promise<void>;
+
+  async mounted () {
+    await this.fetchRepositories()
+  }
+}
+</script>

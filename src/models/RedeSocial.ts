83d6b4a5349ca330a2ac78f7e8d931ffd9d7954b
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-21 15:18:47 -0300

    Melhorar interface de rede social

--- src/models/RedeSocial.ts
+++ src/models/RedeSocial.ts
@@ -2,15 +2,16 @@
  * Descreve as propriedades para uma rede social a ser exibida.
  */
 export interface RedeSocial {
-  username: string
-  profileUrl: URL,
-  redeIcon?: FontAwesomeIcon
+  name: string;
+  username: string;
+  profileUrl: URL;
+  redeIcon?: FontAwesomeIcon;
 }
 
 /**
  * Descreve as propriedades para um ícone do FortAwesome.
  */
 export interface FontAwesomeIcon {
-  faType: 'fas' | 'fab'
-  faIcon: string
+  faType: 'fas' | 'fab';
+  faIcon: string;
 }

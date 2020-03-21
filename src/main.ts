83fd538dd030b0fdaef4a95a826be6da2e09fb14
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-21 12:48:36 -0300

    Configurar o FortAwesome

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faHome, faQuestion } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// Adicionando ícones do FontAwesome padrão
+library.add(faHome, faQuestion)
+
+// Adicionando ícones do FontAwesome Brands
+// library.add(faGithub)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 

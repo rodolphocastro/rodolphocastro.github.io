ca97905b48f132c45fe5b7f3fa4aff26d06afab4
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-21 12:23:01 -0300

    Adicionar component para a barra de navegação

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <router-link :to="{ name: 'Home' }">Home</router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'Sobre' }">Sobre</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({
+  name: 'navigation-bar'
+})
+export default class NavigationBar extends Vue {}
+</script>

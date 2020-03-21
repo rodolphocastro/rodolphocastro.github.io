126f76268138422c0d2990bae5da422ada62883c
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-21 15:19:00 -0300

    Utilizar o state no component de redes sociais

--- src/components/home/RedesSociaisList.vue
+++ src/components/home/RedesSociaisList.vue
@@ -1,35 +1,14 @@
 <template>
   <ul>
-    <li>
+    <li v-for="rede in redesSociais" :key="rede.name">
       <span>
-        <abbr title="GitHub">
-          <font-awesome-icon :icon="['fab', 'github']"></font-awesome-icon>
+        <abbr :title="rede.name">
+          <template v-if="rede.redeIcon">
+            <font-awesome-icon :icon="[rede.redeIcon.faType, rede.redeIcon.faIcon]"></font-awesome-icon>
+          </template>
+          <template v-else>{{rede.name}}</template>
         </abbr>
-        <a href="https://github.com/rodolphocastro">@rodolphocastro</a>
-      </span>
-    </li>
-    <li>
-      <span>
-        <abbr title="LinkedIn">
-          <font-awesome-icon :icon="['fab', 'linkedin']"></font-awesome-icon>
-        </abbr>
-        <a href="https://www.linkedin.com/in/rodolpho-alves93">@rodolpho-alves93</a>
-      </span>
-    </li>
-    <li>
-      <span>
-        <abbr title="Medium">
-          <font-awesome-icon :icon="['fab', 'medium']"></font-awesome-icon>
-        </abbr>
-        <a href="https://medium.com/@rodolpho.castro.a">@rodolpho.castro.a</a>
-      </span>
-    </li>
-    <li>
-      <span>
-        <abbr title="Telegram">
-          <font-awesome-icon :icon="['fab', 'telegram']"></font-awesome-icon>
-        </abbr>
-        <a href="https://t.me/AlvesRC">@AlvesRC</a>
+        <a :href="rede.profileUrl">{{rede.username}}</a>
       </span>
     </li>
   </ul>
@@ -37,15 +16,25 @@
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
+import { mapState, mapActions } from 'vuex'
+import { RedeSocial } from '@/models/RedeSocial'
 
 @Component({
-  name: 'redes-sociais-list'
+  name: 'redes-sociais-list',
+  computed: { ...mapState(['redesSociais']) },
+  methods: { ...mapActions(['fetchRedesSociais']) }
 })
-export default class RedesSociaisList extends Vue {}
+export default class RedesSociaisList extends Vue {
+  fetchRedesSociais!: () => void;
+  redesSociais!: RedeSocial[];
+
+  mounted () {
+    this.fetchRedesSociais()
+  }
+}
 </script>
 
 <style scoped>
-
 svg {
   margin-right: 5px;
 }
@@ -53,5 +42,4 @@ svg {
 a {
   text-decoration: none;
 }
-
 </style>

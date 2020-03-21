934cd57dd999425fcece59091fd7ebf8ac1bd187
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-21 20:25:16 -0300

    Adicionar model de Tecnologia

---
+++ src/models/Tecnologia.ts
@@ -0,0 +1,12 @@
+import { FontAwesomeIcon } from './fontAwesomeIcon'
+
+/**
+ * Tecnologia a ser listada no site.
+ */
+export interface Tecnologia {
+  name: string;
+  description: string;
+  stars: 1 | 2 | 3 | 4 | 5;
+  techUrl: URL;
+  techIcon?: FontAwesomeIcon;
+}

e3c82dd9385b954d68e8cfa7685e326b241d7131
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-20 22:32:38 -0300

    Adicionar limpeza dos compilados ao deploy.psa

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,9 @@
+Remove-Item .\css\ -Recurse -Force
+Remove-Item .\js\ -Recurse -Force
+Remove-Item .\img\ -Recurse -Force
+Remove-Item .\favicon.ico -Force
+Remove-Item .\index.html -Force
+
 yarn install
 
 yarn build

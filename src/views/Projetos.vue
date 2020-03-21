76ec861a4611d5d7df30ea4ee71056002b0af7c3
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-21 17:02:53 -0300

    Buscar e exibir repositórios

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,23 +1,30 @@
 <template>
   <section>
     <h2>Meus Projetos</h2>
-    <p>Estes são os repositórios <em>públicos</em> nos quais participo</p>
-    <article>
-      <h3>W.I.P.</h3>
+    <p>Estes são os repositórios <em>públicos</em> nos quais participo.</p>
+    <small>Eles são recuperados automaticamente do meu perfil no GitHub, então não estão em nenhuma ordem específica!</small>
+    <article v-for="repo in repositories" :key="repo.id">
+      <h3>{{repo.name}}</h3>
+      <small>{{repo.full_name}}</small>
+      <p>{{repo.description}}</p>
+      <a :href="repo.html_url">Abrir no GitHub</a>
     </article>
   </section>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
-import { mapActions } from 'vuex'
+import { mapActions, mapState } from 'vuex'
+import { Repository } from '@/models/github/Repository'
 
 @Component({
   name: 'projetos',
+  computed: { ...mapState('projetos', ['repositories']) },
   methods: { ...mapActions('projetos', ['fetchRepositories']) }
 })
 export default class Projetos extends Vue {
   fetchRepositories!: () => Promise<void>;
+  repositories!: Repository[];
 
   async mounted () {
     await this.fetchRepositories()

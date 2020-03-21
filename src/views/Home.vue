34b33ca8bb1765fca20a05d09062e1b9435f6b06
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-21 12:14:11 -0300

    Adicionar conteúdo à Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,19 @@
 <template>
-  <article>
-
-  </article>
+  <section>
+    <h2>Bem vindo ao meu website!</h2>
+    <article>
+      <h3>Quem sou eu?</h3>
+      <p>
+        Meu nome é <strong>Rodolpho Alves</strong>, atuo como <em>Arquiteto de Soluções</em> com ênfase na nuvem.
+      </p>
+      <p>
+        Sou entusiasta com tecnologias, sempre buscando expandir o repertório de ferramentas disponíveis para solucionar os problemas apresentados por nossos clientes.
+      </p>
+      <p>
+        No tempo livre sou corredor de rua, marombeiro ávido e gamer! <em>(PC, Playstation4 e Nintendo)</em>
+      </p>
+    </article>
+  </section>
 </template>
 
 <script lang='ts'>

041924ac5af5174c98dc09229c57385515b114e9
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-21 14:44:09 -0300

    Extrair component para redes sociais

---
+++ src/components/home/RedesSociaisList.vue
@@ -0,0 +1,57 @@
+<template>
+  <ul>
+    <li>
+      <span>
+        <abbr title="GitHub">
+          <font-awesome-icon :icon="['fab', 'github']"></font-awesome-icon>
+        </abbr>
+        <a href="https://github.com/rodolphocastro">@rodolphocastro</a>
+      </span>
+    </li>
+    <li>
+      <span>
+        <abbr title="LinkedIn">
+          <font-awesome-icon :icon="['fab', 'linkedin']"></font-awesome-icon>
+        </abbr>
+        <a href="https://www.linkedin.com/in/rodolpho-alves93">@rodolpho-alves93</a>
+      </span>
+    </li>
+    <li>
+      <span>
+        <abbr title="Medium">
+          <font-awesome-icon :icon="['fab', 'medium']"></font-awesome-icon>
+        </abbr>
+        <a href="https://medium.com/@rodolpho.castro.a">@rodolpho.castro.a</a>
+      </span>
+    </li>
+    <li>
+      <span>
+        <abbr title="Telegram">
+          <font-awesome-icon :icon="['fab', 'telegram']"></font-awesome-icon>
+        </abbr>
+        <a href="https://t.me/AlvesRC">@AlvesRC</a>
+      </span>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({
+  name: 'redes-sociais-list'
+})
+export default class RedesSociaisList extends Vue {}
+</script>
+
+<style scoped>
+
+svg {
+  margin-right: 5px;
+}
+
+a {
+  text-decoration: none;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,61 +17,20 @@
     <article>
       <h3>Onde me encontrar</h3>
       <p>Estou presente nestas redes sociais:</p>
-      <ul>
-        <li>
-          <span>
-            <abbr title="GitHub">
-              <font-awesome-icon :icon="['fab', 'github']"></font-awesome-icon>
-            </abbr>
-            <a href="https://github.com/rodolphocastro">@rodolphocastro</a>
-          </span>
-        </li>
-        <li>
-          <span>
-            <abbr title="LinkedIn">
-              <font-awesome-icon :icon="['fab', 'linkedin']"></font-awesome-icon>
-            </abbr>
-            <a href="https://www.linkedin.com/in/rodolpho-alves93">@rodolpho-alves93</a>
-          </span>
-        </li>
-        <li>
-          <span>
-            <abbr title="Medium">
-              <font-awesome-icon :icon="['fab', 'medium']"></font-awesome-icon>
-            </abbr>
-            <a href="https://medium.com/@rodolpho.castro.a">@rodolpho.castro.a</a>
-          </span>
-        </li>
-        <li>
-          <span>
-            <abbr title="Telegram">
-              <font-awesome-icon :icon="['fab', 'telegram']"></font-awesome-icon>
-            </abbr>
-            <a href="https://t.me/AlvesRC">@AlvesRC</a>
-          </span>
-          </li>
-      </ul>
+      <redes-sociais-list></redes-sociais-list>
     </article>
   </section>
 </template>
 
 <script lang='ts'>
 import { Component, Vue } from 'vue-property-decorator'
+import RedesSociaisList from '@/components/home/RedesSociaisList.vue'
 
 @Component({
-  name: 'home'
+  name: 'home',
+  components: {
+    RedesSociaisList
+  }
 })
 export default class Home extends Vue {}
 </script>
-
-<style scoped>
-
-svg {
-  margin-right: 5px;
-}
-
-a {
-  text-decoration: none;
-}
-
-</style>

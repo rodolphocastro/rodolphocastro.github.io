99a0c74eaf9af4e6ac32c2d4b6fc0ed71351cf41
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-22 16:37:18 -0300

    Adicionar os components à página Curriculo

--- src/views/Curriculo.vue
+++ src/views/Curriculo.vue
@@ -5,15 +5,31 @@
       Esta é uma versão resumido do meu currículo. A versão completa pode ser encontrada
       <a href="https://www.linkedin.com/in/rodolpho-alves93/">no meu LinkedIn</a>.
     </p>
-    <!-- TODO: Adicionar articles -->
+    <sobre></sobre>
+    <experiencia></experiencia>
+    <formacao></formacao>
+    <certificacoes></certificacoes>
+    <idiomas></idiomas>
   </section>
 </template>
 
 <script lang='ts'>
 import { Vue, Component } from 'vue-property-decorator'
+import Sobre from '@/components/curriculo/Sobre.vue'
+import Experiencia from '@/components/curriculo/Experiencia.vue'
+import Formacao from '@/components/curriculo/Formacao.vue'
+import Certificacoes from '@/components/curriculo/Certificacoes.vue'
+import Idiomas from '@/components/curriculo/Idiomas'
 
 @Component({
-  name: 'curriculo'
+  name: 'curriculo',
+  components: {
+    Sobre,
+    Experiencia,
+    Formacao,
+    Certificacoes,
+    Idiomas
+  }
 })
 export default class Curriculo extends Vue {}
 </script>

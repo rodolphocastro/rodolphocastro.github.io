74096829fd132c8ab191dccd545b07aa9306a3a7
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-22 15:46:58 -0300

    Adicionar base da view de Currículos

---
+++ src/views/Curriculo.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <h2>Meu Currículo</h2>
+    <p>
+      Esta é uma versão resumido do meu currículo. A versão completa pode ser encontrada
+      <a href="https://www.linkedin.com/in/rodolpho-alves93/">no meu LinkedIn</a>.
+    </p>
+    <!-- TODO: Adicionar articles -->
+  </section>
+</template>
+
+<script lang='ts'>
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({
+  name: 'curriculo'
+})
+export default class Curriculo extends Vue {}
+</script>

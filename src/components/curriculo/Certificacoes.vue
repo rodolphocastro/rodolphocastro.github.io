9de01af948f471af75c355f55af93fcc77ab278c
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-03-22 16:37:07 -0300

    Adicionar components do Currículo

---
+++ src/components/curriculo/Certificacoes.vue
@@ -0,0 +1,41 @@
+<template>
+  <article>
+    <h3>Certificações</h3>
+    <dl>
+      <dt>Microsoft Certified: Azure Solutions Architect Expert (10/2019)</dt>
+      <dd>
+        <p>Esta certificação demonstra conhecimento das técnicas e conhecimento necessários para conversar com <em>stakeholders</em> e auxiliar empresas a traduzirem seus requisitos em soluções <strong>Seguras, Escaláveis e Confiáveis</strong></p>
+        <p>Candidatos desta certificação possuem experiência e conhecimento avançado em vários aspectos de <em>T.I.</em>, como <strong>redes, virtualização, identidade, segurança, resiliência de sistemas, recuperação de desastres, gestão de dados, orçamentos e governança</strong>.</p>
+        <p>
+          <a
+            href="https://www.youracclaim.com/badges/b558173e-8725-4c10-a41e-8c91548117b9/linked_in_profile"
+          >Mais informações</a>
+        </p>
+      </dd>
+      <dt>Microsoft Certified: Azure Developer Associate (03/2020)</dt>
+      <dd>
+        <p>Valida as técnicas e conhecimentos necessários para desenhar e construir soluções na nuvem, como aplicações e serviços</p>
+        <p>Candidatos desta certificação participam em todas as fases do desenvolvimento, desde o design da solução à manutenção.</p>
+        <p>
+          <a
+            href="https://www.youracclaim.com/badges/b756fcea-527b-4c1d-b785-4ee325ddef4b/linked_in_profile"
+          >Mais Informações</a>
+        </p>
+      </dd>
+      <dt>Scrum Fundamentals Certified (01/2020)</dt>
+      <dd>
+        <p>Valida o conhecimento sobre os fundamentos da metodologia ágil <strong>SCRUM</strong>.</p>
+        <p>Emitido pela <a href="https://www.scrumstudy.com/portuguese">SCRUMstudy</a></p>
+      </dd>
+    </dl>
+  </article>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({
+  name: 'certificacoes'
+})
+export default class Certificacoes extends Vue {}
+</script>

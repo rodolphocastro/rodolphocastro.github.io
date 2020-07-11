1499b8583969a6fd7c654dd04ce34d7c608bde90
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-07-11 12:05:37 -0300

    feat: Adicionar a View para os Posts

---
+++ src/views/Posts.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>Work In Progress</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import { mapActions, mapState } from 'vuex'
+import { Post } from '@/models/devto/Post'
+
+@Component({
+  name: 'posts',
+  computed: { ...mapState('posts', ['posts']) },
+  methods: { ...mapActions('posts', ['fetchPosts']) }
+})
+export default class Posts extends Vue {
+  fetchPosts!: () => Promise<void>;
+  posts!: Post[]
+
+  async mounted () {
+    await this.fetchPosts()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

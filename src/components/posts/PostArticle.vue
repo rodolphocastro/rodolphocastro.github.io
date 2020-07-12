47cba9e9ccd47c288dadf7684c9a79e5c157e332
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-07-11 21:00:11 -0300

    feat: Exibir os Posts encontrados
    Fixes #6

---
+++ src/components/posts/PostArticle.vue
@@ -0,0 +1,34 @@
+<template>
+  <article>
+    <h3>
+      {{post.title}}
+      <small> ({{post.readable_publish_date}})</small>
+    </h3>
+    <img :src="post.social_image" alt="Post's Social Image" />
+    <p>{{post.description}}</p>
+    <p>Tags: {{post.tag_list.join(', ')}}</p>
+    <a :href="post.url">Open on Dev.To</a>
+  </article>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+import { Post } from '@/models/devto/Post'
+
+@Component({
+  name: 'post-article'
+})
+export default class PostArticle extends Vue {
+  @Prop({ required: true }) readonly post!: Post;
+}
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+
+h3 {
+  margin-bottom: 1em;
+}
+</style>
--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -1,7 +1,18 @@
 <template>
-  <div>
-    <p>Work In Progress</p>
-  </div>
+  <section>
+    <h2>Posts</h2>
+    <p>
+      Estes são meus posts publicados no <a href="https://dev.to/ardc_overflow">Dev.To</a>
+    </p>
+    <small>Recuperados automaticamente através da API do Dev.To</small>
+    <hr/>
+    <template v-if="posts.length">
+      <post-article v-for="post in posts" v-bind:key="post.id" :post="post"></post-article>
+    </template>
+    <template v-else>
+      <p>Nada foi encontrado!</p>
+    </template>
+  </section>
 </template>
 
 <script lang="ts">
@@ -11,6 +22,9 @@ import { Post } from '@/models/devto/Post'
 
 @Component({
   name: 'posts',
+  components: {
+    'post-article': () => import('@/components/posts/PostArticle.vue')
+  },
   computed: { ...mapState('posts', ['posts']) },
   methods: { ...mapActions('posts', ['fetchPosts']) }
 })

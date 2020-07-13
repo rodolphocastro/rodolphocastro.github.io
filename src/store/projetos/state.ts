b5480d73f68ddbf37e0c43365a83987c26c9f03b
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-07-12 21:00:31 -0300

    feat: Adicionar loading aos Projetos
    FIxes #14

--- src/store/projetos/state.ts
+++ src/store/projetos/state.ts
@@ -2,8 +2,10 @@ import { Repository } from '@/models/github/Repository'
 
 export interface ProjetosStateStorable {
   repositories: Repository[];
+  loading: boolean;
 }
 
 export const state: ProjetosStateStorable = {
-  repositories: []
+  repositories: [],
+  loading: false
 }

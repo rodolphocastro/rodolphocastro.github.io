dcbcfeb6a20777a77e8286cf07a31a63baf8595f
Rodolpho Alves <rodolpho.castro.a+macosx@gmail.com>
2024-03-21 21:25:26 -0300

    chore: stop linting env.d.ts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,33 +1,42 @@
 module.exports = {
-    env: {
-      node: true,
-      es2022: true,
-      browser: true,
-    },
-    extends: ['eslint:recommended', 'plugin:astro/recommended'],
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
-    rules: {},
-    overrides: [
-      {
-        files: ["*.astro"],
-        parser: "astro-eslint-parser",
-        parserOptions: {
-          parser: "@typescript-eslint/parser",
-          extraFileExtensions: [".astro"],
-        },
-        rules: {},
+  env: {
+    node: true,
+    es2022: true,
+    browser: true,
+  },
+  extends: ['eslint:recommended', 'plugin:astro/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
       },
-      {
-        files: ['*.ts'],
+      rules: {},
+    },
+    {
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      extends: ['plugin:@typescript-eslint/recommended'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' }],
+        '@typescript-eslint/no-non-null-assertion': 'off',
+      },
+    },
+    {
+      files: ['env.d.ts'],  // ignoring the env.d.ts file
+      parserOptions: {
         parser: '@typescript-eslint/parser',
-        extends: ['plugin:@typescript-eslint/recommended'],
-        rules: {
-          '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' }],
-          '@typescript-eslint/no-non-null-assertion': 'off',
-        },
       },
-    ],
-  };
+      rules: {
+        '@typescript-eslint/triple-slash-reference': 'off',  // disable triple-slash-reference rule for env.d.ts
+      },
+    }
+  ],
+};

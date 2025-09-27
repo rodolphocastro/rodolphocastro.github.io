93816a81bf9f66cb6d0efde33902a9f32919ac72
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2025-09-26 21:15:11 -0300

    Fix/posts-reload (#123)
    * fix: prevent full reload while opening a post
    * refactor: use vue composition API for ArticleList
    * chore: bump to 1.0.4

--- .vitepress/theme/ArticleList.vue
+++ .vitepress/theme/ArticleList.vue
@@ -1,48 +1,42 @@
 <template>
   <section class="article-list">
-    <div class="row" v-for="row in rows" :key="row">
-      <article class="col" v-for="post in row" :key="post.id">
+    <div class="row" v-for="row in rows">
+      <article class="col" v-for="post in row" :key="post.url">
         <ArticleCard :post="post" />
       </article>
     </div>
   </section>
 </template>
 
-<script>
+<script setup lang="ts">
+import { defineProps, computed } from 'vue';
+import { BlogPost } from '../../utils.mts';
 import ArticleCard from './ArticleCard.vue';
 
-export default {
-  name: 'ArticleList',
-  components: {
-    ArticleCard,
-  },
-  props: {
-    posts: {
-      type: Array,
-      required: true,
-    },
-  },
-  computed: {
-    rows() {
-      // TODO: this area is a bit messy, needs refactor
-      // Sort posts by date descending
-      const sortedPosts = this.posts.sort(
-        (a, b) =>
-          new Date(b.postDate).valueOf() - new Date(a.postDate).valueOf(),
-      );
-      const rows = [];
-      let currentRow = [];
-      for (let i = 0; i < sortedPosts.length; i++) {
-        currentRow.push(sortedPosts[i]);
-        if (currentRow.length === 2 || i === sortedPosts.length - 1) {
-          rows.push(currentRow);
-          currentRow = [];
-        }
-      }
-      return rows;
-    },
-  },
-};
+/**
+ * Props required for this component.
+ */
+const props = defineProps<{
+  posts: BlogPost[];
+}>();
+
+const rows = computed(() => {
+  // TODO: this area is a bit messy, needs refactor
+  // Sort posts by date descending
+  const sortedPosts = props.posts.sort(
+    (a, b) => new Date(b.postDate).valueOf() - new Date(a.postDate).valueOf(),
+  );
+  const rows = [];
+  let currentRow = [];
+  for (let i = 0; i < sortedPosts.length; i++) {
+    currentRow.push(sortedPosts[i]);
+    if (currentRow.length === 2 || i === sortedPosts.length - 1) {
+      rows.push(currentRow);
+      currentRow = [];
+    }
+  }
+  return rows;
+});
 </script>
 
 <style scoped>
